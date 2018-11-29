133811cb7f1f97615f107eb52e6348751b7bcbbf
scalaview <chailink100@gmail.com>
2018-11-29 15:30:13 +0800

    update script

--- client.js
+++ client.js
@@ -30,9 +30,6 @@ App.unpack = function(behind_id){
 App.set_up = function(front_ids){
   data = {
     front_ids: front_ids,
-    // broken: {
-    //   front_id: 1
-    // },
     action : "set_up"
   };
   message = {
@@ -47,7 +44,7 @@ App.connect_server = function() {
   const W3CWebSocket = require('ws');
   App.ws = new W3CWebSocket('ws://localhost:3004/wall', ["actioncable-v1-json", "actioncable-unsupported"]);
 
-  App.param = {channel: "OperationChannel", uuid: guid()};
+  App.param = {channel: "OperationChannel", uuid: guid(), warehouse_id: 1};
 
   App.ws.on('open', function open() {
     data = {
@@ -57,18 +54,20 @@ App.connect_server = function() {
     App.ws.send(JSON.stringify(data))
   });
   App.ws.on('message', function (event) {
-    console.log(JSON.parse(event))
+    console.log(event)
     var response = JSON.parse(event)
     if(response.message && response.message.action && response.message.action == 'pick'){
       if(response.message.data[0]){
-        App.pick(response.message.data[0].front_id)
+        for(var d in response.message.data){
+          App.pick(response.message.data[d].front_id)
+        }
       }
     }
     if(response.message && response.message.action && response.message.action == 'unpack'){
       App.unpack(response.message.data.behind_id)
     }
     if(response.message && response.message.action && response.message.action == 'set_up'){
-      App.set_up([1, 2, 3, 4, 5])
+      App.set_up([1, 2, 10, 11, 13, 15, 16])
     }
   });
   function guid() {
--- scanner.js
+++ scanner.js
@@ -1,29 +1,53 @@
 var request = require('request');
 
+var product_ids = [
+  'PPP366664',
+  'PPP366662',
+  'PPP366661',
+  'PPP366660',
+  'PPP366162',
+  'PPP362838',
+  'PPP358215',
+  'PPP357619',
+  'PPP357618',
+  'PPP353918',
+  'PPP309059',
+  'PPP307283',
+  'PPP307282',
+  'PPP307281',
+  'PPP305645' ]
 
-var options = {
-  uri: 'http://localhost:3004/v1.0/tag_boxes/pick',
-  method: 'POST',
-  json: {
-    package_group_id: "XAH174738",
-    product_id: "888620038708",
-    wall_id: "XAK1"
+var index = 0
+
+function make_req() {
+  if(index >= product_ids.length ){// product_ids.length){
+    clearInterval(myVar);
+    console.log("finish")
+    return
   }
-};
-
-
-request(options, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body)
-  }
-});
-
-
-
+  var options = {
+    uri: 'http://localhost:3004/v1.0/tag_boxes/pick',
+    method: 'POST',
+    json: {
+      package_group_id: "XAH174738",
+      product_id: product_ids[index],
+      wall_id: "XAK1"
+    }
+  };
+  index++
+  console.log(options)
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body)
+    }
+  });
+}
+console.log("start")
+var myVar = setInterval(function(){ make_req() }, 3000);
 
 // {
 //     package_group_id: "XAH174738",
-//     product_id: "PPP362838",
+//     product_id: "PPP305645",
 //     wall_id: "XAK1"
 //   }
 
@@ -40,6 +64,8 @@ request(options, function (error, response, body) {
 //     product_id: "PPP366162",
 //     wall_id: "XAK1"
 //   }
+
+
 
 
 // package_group_id: "XAH174738",
